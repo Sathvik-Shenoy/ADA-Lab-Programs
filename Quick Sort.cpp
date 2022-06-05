57b592ae19cd432061d7463451dfadf7441cb2c2
Sathvik-Shenoy <sathvikshenoyb@gmail.com>
2022-06-05 22:08:52 +0530

    Modified quick sort

--- Quick Sort.cpp
+++ Quick Sort.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<ctime>
+//#include<stdlib.h>
 using namespace std;
 int partition(int a[],int f,int l)
 {
@@ -38,9 +40,21 @@ void quicksort(int a[],int f,int l)
 }
 int main()
 {
-    int a[]={4,3,2,1};
-    quicksort(a,0,4);
-    for(int i=0;i<4;i++)
-        cout<<a[i];
+    int n=10000;
+    int i,k=1;
+    double time;
+    while(k<=50)
+    {
+        int a[n];
+        for(i=0;i<n;i++)
+            a[i]=rand()%1000;
+        clock_t start=clock();
+        quicksort(a,0,n);
+        clock_t end=clock();
+        time=(double)(end-start)/CLOCKS_PER_SEC;
+        cout<<n<<"\t"<<time<<"\n";
+        n=n+10000;
+        k++;
+    }
     return 0;
 }
